97c1da6a85d2e9987d90388537a812b64f537b8d
Sreerag Sree <sreeragappu421@gmail.com>
2024-03-26 11:00:25 +0530

    first commit

---
+++ Copy a File.py
@@ -0,0 +1,2 @@
+from shutil import copyfile
+copyfile("C:/Users/sreerag/PycharmProjects/hiii.txt","C:/Users/sreerag/PycharmProjects/hello.txt")
---
+++ Sort a Dictinary by Value.py
@@ -0,0 +1,11 @@
+marks ={"jhon":23,"lisa":56,"sid":48}
+print (marks)
+# sv=sorted(marks.items(),key=lambda x : x[1])
+# print(sv)
+
+
+
+
+
+v = sorted(marks.values())
+print(v)
